8ddf0cec812a08283335ec08f0117c4b2fe8b1d8
bhlezzdmplg <yharrasallyguiao@gmail.com>
2024-11-05 10:24:13 +0800

    Create classSubjects.js

---
+++ classSubjects.js
@@ -0,0 +1,9 @@
+// Collecting class information and storing it in variables
+let subjectTitle = "DSA";
+let classSchedule = "8:00 AM - 9:30 AM, Tue-Thur";
+let classroom = "Room 121";
+let classInstructor = "Mr. VJ";
+let studentName = "Bhlezz Garbin";
+
+// Logging the information in the specified format
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at ${classroom}. The instructor for the subject is ${classInstructor}.`);
